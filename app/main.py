f17a9be921325975972d05d7dc1d5d7562ae7e07
akovyakh <anko@teknologisk.dk>
2021-01-18 17:35:51 +0100

    cleaned repo a bit

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
-# import ast
 
 app = Flask(__name__)
-app.config["Debug"] =True
 
+#load data from csv into memory
 total_tests = pd.read_csv(
     "./data/downsampled_data.csv"
-    # "C:/Users/anko/Documents/git_staging/flask/covid_flask_api/data/downsampled_data.csv"
     ).set_index('year_week')
 
 @app.errorhandler(404)
@@ -19,33 +17,15 @@ def pageNotFound(e):
 def badRequest(e):
     return "<h1> Bad request <h1>", 400
 
+#landing page
 @app.route("/")
 def home():
     return "Hello there! General Kenobi...", 200
 
-# @app.route("/country_tests_done", methods=["GET"])
-# def get_all_data():
-#     data = pd.read_csv("C:/Users/anko/Documents/git_staging/flask/flask_app/data/downsampled_data.csv")
-#     data = data.to_dict()
-#     return {'data': data}, 200
-
-# @app.route('/json/<uuid>', methods=['GET', 'POST'])
-# def add_message(uuid):
-#     content = request.json
-#     print(content['mytext'])
-#     return jsonify({"uuid":uuid})
-
-#  curl -X GET -H 'Content-Type: application/json' http://127.0.0.1:5000/country_tests_done -d '{"country": "DK","country":"DK"}'
-
+# api endpoint for receiving testing data with some typical error checking
 @app.route('/country_tests_done', methods=['GET'])                                                                                                    
 def all_tests():                                                                                                                              
     data = request.get_json()
-
-    print(len(data.keys()))
-    # print(type(data))
-    # print(data.get("country"))
-    # for (k,v) in data.items():
-    #     print(k,v)
     
     if len(data.keys())>1:
         return badRequest(400)
@@ -55,7 +35,6 @@ def all_tests():
             return pageNotFound(404)
 
     response_json = total_tests.tests_done.loc[(total_tests.country_code) == data.get("country")]
-
     return response_json.to_json()
 
 
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
+# Test using requests lib from python when running flask api locally
+
 import requests
-res = requests.post('http://localhost:5000/api/echo-json', json={"donkeu":"DK"})
+res = requests.post('http://localhost:5000/country_tests_done', json={"country":"ES"})
 if res.ok:
     print(res.json())
